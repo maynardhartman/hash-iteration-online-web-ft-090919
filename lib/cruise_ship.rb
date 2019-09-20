931c28a574616ac51e142944e29c41f004ab1150
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-20 06:14:07 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,15 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
-
+passengers = {
+suite_a: "Amanda Presley", 
+suite_b: "Seymour Hoffman", 
+suite_c: "Alfred Tennyson", 
+suite_d: "Charlie Chaplin", 
+suite_e: "Crumpet the Elf"
+}
+winner = ""
 def select_winner(passengers)
   # add the code snippet here!
+  psddengers.each { |suite, name|
+    if suite == :suite_a && name.start_with("A")
+      winner = name 
+    end
 end
